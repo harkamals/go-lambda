fb816ad061f490e868a3e560f48a8fed04aa447d
Harkamal Singh <harkamals@gmail.com>
2018-06-01 22:34:01 +0800

    initial

---
+++ main.go
@@ -0,0 +1,32 @@
+package main
+
+
+
+import (
+	"fmt"
+	"github.com/aws/aws-lambda-go/lambda"
+)
+
+// Request ...
+type Request struct {
+	ID    float64 `json:"id"`
+	Value string  `json:"value"`
+}
+
+// Response ..
+type Response struct {
+	Message string `json:"message"`
+	OK      bool   `json:"ok"`
+}
+
+// Handler ...
+func Handler(request Request) (Response, error){
+	return Response{
+		Message: fmt.Sprintf("Processed request %f", request.ID),
+		OK:      true,
+	}, nil
+}
+
+func main() {
+	lambda.Start(Handler)
+}
